8e3cdba3c3c637c5a7b9756c67630378dd614678
JG Yan <yanjingguan@hotmail.com>
2018-11-21 10:11:57 +0800

    补提一个版本

--- MONO/MONO/Main/YanBaseViewController.swift
+++ MONO/MONO/Main/YanBaseViewController.swift
@@ -14,6 +14,8 @@ class YanBaseViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
